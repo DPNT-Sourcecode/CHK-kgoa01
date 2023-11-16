764bbb1fc56a4f29be39691731419b5c468426a2
sbx_user1051 <sbx_user1051@169.254.40.21>
2023-11-16 22:16:29 +0000

    Thu Nov 16 22:16:29 UTC 2023

--- src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -10,18 +10,8 @@ namespace BeFaster.App.Tests.Solutions.SUM
         [TestCase(1, 1, ExpectedResult = 2)]
         public int ComputeSum(int x, int y)
         {
-            //Assert.AreEqual(1, 2);
             return SumSolution.Sum(x, y);
         }
     }
-
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            
-        }
-    }
 }
+
