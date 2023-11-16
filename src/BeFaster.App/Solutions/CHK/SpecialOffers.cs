9f230111d1ffa7bdd3314b994bcc457db29d8e1a
sbx_user1051 <sbx_user1051@169.254.40.21>
2023-11-16 22:38:27 +0000

    Thu Nov 16 22:38:27 UTC 2023

---
+++ src/BeFaster.App/Solutions/CHK/SpecialOffers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    internal class SpecialOffers
+    {
+        public Item ItemOffer { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
