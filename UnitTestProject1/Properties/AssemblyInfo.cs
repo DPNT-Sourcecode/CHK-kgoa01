ea006acded61df43dddd195699cc993cb37c55d9
sbx_user1051 <sbx_user1051@169.254.40.21>
2023-11-16 22:04:30 +0000

    Thu Nov 16 22:04:30 UTC 2023

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ca408b8-5715-4151-8bde-0820901ea586")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -0,0 +1,27 @@
+﻿using BeFaster.App.Solutions.SUM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace BeFaster.App.Tests.Solutions.SUM
+{
+    [TestFixture]
+    public class SumSolutionTest
+    {
+        [TestCase(1, 1, ExpectedResult = 2)]
+        public int ComputeSum(int x, int y)
+        {
+            //Assert.AreEqual(1, 2);
+            return SumSolution.Sum(x, y);
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
